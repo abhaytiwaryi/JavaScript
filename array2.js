d08ba3fa0fd8ac5a4d5dd3407d84431e2e939583
Abhay Kumar Tiwary <abhaykumartiwary@Abhays-MBP.bbrouter>
2025-07-07 05:16:59 +0530

    updated end

--- array2.js
+++ array2.js
@@ -19,3 +19,16 @@ const anotherArray = [1, 2, 3, [4, 5, 6, [7, 8, 9]]]
 const real_another_array = anotherArray.flat(Infinity)
 console.log(real_another_array);
 
+console.log(Array.from("Abhay Kumar Tiwary"));
+
+// console.log(Array.from({name: "Abhay"})); //Interesting this will retrurn empty array.
+
+let score = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score, score2, score3));
+
+
+
+
