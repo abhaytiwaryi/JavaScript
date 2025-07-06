b510ea57e0b045e51aeceea0ea577daaeccd84d3
Abhay Kumar Tiwary <abhaykumartiwary@Abhays-MBP.bbrouter>
2025-07-06 14:48:44 +0530

    update

--- array.js
+++ array.js
@@ -1,5 +1,5 @@
-const myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-console.log(myArray[0]);
+const myArray = [0, 1, 2, 3, 4, 5,]
+// console.log(myArray[0]);
 
 // const myArr = new Array(1, 2, 3, 4)
 // console.log(myArr[0])
@@ -11,5 +11,19 @@ console.log(myArray[0]);
 // myArray.pop()
 // myArray.unshift(8)
 // myArray.shift()
-console.log(myArray.includes(9))
-console.log(myArray);
+// console.log(myArray.includes(4));
+// console.log(myArray.indexOf(0));
+
+console.log("A ", myArray);
+
+// const newArray = myArray.join()
+// console.log(typeof newArray);
+
+const myn1 = myArray.slice(1, 3);
+console.log(myn1);
+
+console.log("B ", myArray);
+
+const myn2 = myArray.splice(1, 3);
+console.log(myn2);
+console.log("C ", myArray);
