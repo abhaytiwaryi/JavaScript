f0ad85d3b6fe7cb6568f5c4b27575e6ad0166d56
Abhay Kumar Tiwary <abhaykumartiwary@Abhays-MBP.bbrouter>
2025-07-07 12:50:45 +0530

    updated code

---
+++ objects.js
@@ -0,0 +1,18 @@
+//singleton
+// Object.create
+//object literals
+
+const JSuser = {
+    name: "abhay",
+    "Full Name": "Abhay Kumar",
+    age: 18,
+    location: "India",
+    email: "example.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Tuesday", "Wednesday"],
+}
+
+console.log(JSuser.age);
+console.log(JSuser["email"]);
+console.log(JSuser["Full Name"]);
+
