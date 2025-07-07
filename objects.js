ae8bfef1b8c91bfc0a7fade880eb58088c96c049
Abhay Kumar Tiwary <abhaykumartiwary@Abhays-MBP.bbrouter>
2025-07-07 13:41:06 +0530

    update

--- objects.js
+++ objects.js
@@ -12,7 +12,27 @@ const JSuser = {
     lastLoginDays: ["Monday", "Tuesday", "Wednesday"],
 }
 
-console.log(JSuser.age);
-console.log(JSuser["email"]);
-console.log(JSuser["Full Name"]);
+// console.log(JSuser.age);
+// console.log(JSuser["email"]);
+// console.log(JSuser["Full Name"]);
+// JSuser.email = "abhaytiwary@gmail.com"
+// console.log(JSuser.email);
+// Object.freeze(JSuser); // prevents any changes to the object
+// JSuser.age = 20; // this will not change the age
+// console.log(JSuser.age); // still 18
 
+//  const mySym = Symbol("key1")
+// console.log(mySym);
+
+// const mySym = Symbol("key1");
+
+// JSuser[mySym] = "value1",
+// console.log(mySym);
+
+// JSuser.greeting = function() {
+    // console.log("Hello JS User");
+// }
+
+// console.log(JSuser.greeting()); // outputs: Hello JS User
+
+console.log(` Hey There ${JSuser.age}`);
