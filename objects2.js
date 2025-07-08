7d20f05314ace1e379a6f874c5b4a4315ae165c1
Abhay Kumar Tiwary <abhaykumartiwary@Abhays-MBP.bbrouter>
2025-07-08 15:28:39 +0530

    updated

--- objects2.js
+++ objects2.js
@@ -20,7 +20,24 @@
 
 const obj1 = {1: "a", 2: "b"};
 const obj2 = {4: "d", 5: "e"};
+const obj4 = {3: "c", 6: "f"};
 
 // const obj3 = {obj1, obj2}
-const obj3 = Object.assign({}, obj1, obj2) // Merging obj2 into obj1
-console.log(obj3);
+// const obj3 = Object.assign({}, obj1, obj2, obj4) // Merging obj2 into obj1
+// console.log(obj3);
+// console.log(obj1);
+
+const users = [
+    {id: 1, name: "Abhay Tiwary", age: 22},
+    {id: 2, name: "John Doe", age: 25},
+    {id: 3, name: "Jane Smith", age: 30},
+
+    {id: 1, name: "Abhay Tiwary", age: 22},
+    {id: 2, name: "John Doe", age: 25},
+    {id: 3, name: "Jane Smith", age: 30},
+
+    {id: 1, name: "Abhay Tiwary", age: 22},
+    {id: 2, name: "John Doe", age: 25},
+    {id: 3, name: "Jane Smith", age: 30}
+]
+
