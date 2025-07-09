4304c5e7c5f94347b23b3bf9756ba9e4eb5f0279
Abhay Kumar Tiwary <abhaykumartiwary@Abhays-MBP.bbrouter>
2025-07-09 20:09:11 +0530

    update

---
+++ functionsAndParameter.js
@@ -0,0 +1,22 @@
+// console.log("Function Parameters Example");
+
+function greet() {
+console.log("A");
+ console.log("b");
+ console.log("h");
+ console.log("a");
+ console.log("y");
+}
+
+// greet()
+
+
+function addTwoNumbers(num1, num2) {
+  console.log("The sum is: " + (num1 + num2));
+}
+
+// addTwoNumbers(5, 10);
+
+const result = addTwoNumbers(5, 10);
+// console.log(result);
+
