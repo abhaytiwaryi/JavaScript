781d5d8c01f2288b41e8a9c429e17861adb78967
Abhay Kumar Tiwary <abhaykumartiwary@Abhays-MBP.bbrouter>
2025-07-21 19:44:25 +0530

    updated

--- fnWithObjectAndArray.js
+++ fnWithObjectAndArray.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!")
+function CalculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(CalculateCartPrice(100, 200, 300, 400, 500))
+
+const user = {
+    username: "Abhay",
+    prcie: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.prcie}`);
+    
+}
+handleObject(user)
